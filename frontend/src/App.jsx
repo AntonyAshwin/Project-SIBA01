52cf310606b83350327c919f998de160b257aaab
Antony Ashwin <antonyashwin555@gmail.com>
2025-04-08 21:50:46 +0530

    set frontend skelton

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div className="flex items-center justify-center h-screen text-2xl font-bold">
+        Bill Split App Coming Soon 🚀
+      </div>
+    )
+  }
+  
+  export default App
+  
